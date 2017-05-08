cfea55ea18b9d3d69ac6f94d0dc710eea9059a29
Mathew Paschall <XDarkZero89@hotmail.com>
2017-05-08 02:20:39 -0400

    Add files via upload

---
+++ 4_18/4_18/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4_18
+{
+    public partial class Form1 : Form
+    {
+        int remain;
+        int num1;
+        int num2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int getRemainder(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                remain = num1 % num2;
+            }
+            else if (num2 > num1)
+            {
+                remain = num2 % num1;
+            }
+            return remain;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "";
+            num1 = Convert.ToInt32(textBox1.Text);
+            num2 = Convert.ToInt32(textBox2.Text);
+            int remainder = getRemainder(num1, num2);
+            int aRem = 0;
+            while (remainder != 0)
+            {
+                num1 = num2;
+                num2 = remainder;
+                remainder = getRemainder(num1, num2);
+                textBox1.Text = (num1.ToString());
+                textBox2.Text = (num2.ToString());
+            }
+            if (remainder == 0)
+            {
+                textBox3.Text = num2.ToString();
+            }
+        }
+    }
+}
---
+++ 4_19/4_19/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4_19
+{
+    public partial class Form1 : Form
+    {
+        double n = 0;
+        double dec = 0;
+        double len = 0;
+        int ind = 0;
+        double ten = 10;
+        double tempTen = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            n = Convert.ToDouble(textBox1.Text);
+            dec = (1 / n);
+            textBox2.Text = dec.ToString();
+            while (ten % n != 1)
+            {
+                if (ten % n == 1)
+                {
+                    tempTen = (ten * 10);
+                    ten = tempTen;
+                    
+                }
+                else
+                {
+                    len = ten;
+                    textBox3.Text = len.ToString();
+                    break;
+                }
+            }
+            
+        }
+    }
+}
