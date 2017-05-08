c43548d218c2fb93da019322b4b447427cd6ab9d
Mathew Paschall <XDarkZero89@hotmail.com>
2017-05-08 02:22:08 -0400

    Add files via upload

---
+++ 12_23/12_23/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your word.");
+            string entered = Console.ReadLine();
+            int len = 1;
+            string word = "";
+            List<string> arr1 = new List<string>();
+            List<string> arr2 = new List<string>();
+
+            for (int i = 0; i < entered.Length; i++)
+                {
+                word = entered.Substring(i, len);
+                arr1.Add(word);
+            }
+            for (int j = arr1.Count-1; j <= entered.Length && j != -1; j--)
+            {
+                word = arr1[j];
+                arr2.Add(word);
+            }
+            for (int h = 0; h < entered.Length; h++)
+            {
+                Console.WriteLine("arr1 = " + arr1[h]);
+                Console.WriteLine("arr2 = " + arr2[h]);
+            }
+            bool isEqual = Enumerable.SequenceEqual(arr1, arr2);
+            if (isEqual == true)
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            { 
+                Console.WriteLine("Not Palindrome");
+            }
+
+
+        }
+    }
+}
---
+++ 12_25/12_25/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _12_25
+{
+    public partial class Form1 : Form
+    {
+        string word, post, combined;
+        int len = 1;
+        int count = 0;
+
+        Stack<string> stack = new Stack<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            post = textBox2.Text;
+
+            while (count < 1)
+            {                
+                for (int i = post.Length - 1; i >= 0; i--)
+                {
+                    word = textBox1.Text;
+                    stack.Push(post.Substring(i, len));
+                }
+                count++;
+            }
+            if (count == 1)
+            {
+                word = combined;
+            }
+
+            if (stack.Count != 0)
+            {
+
+                combined = (word + "" + stack.Pop());
+                textBox3.Text = combined;
+            }
+            
+        }
+    }
+}
